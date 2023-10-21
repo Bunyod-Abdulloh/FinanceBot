3d21678abfd44f8d9756fdcee21e20f18225c93c
Bunyod-Abdulloh <90672691+Bunyod-Abdulloh@users.noreply.github.com>
2023-10-21 05:35:09 +0500

    Updated

--- handlers/all/sample.py
+++ handlers/all/sample.py
@@ -15,3 +15,96 @@
 #              f'\n\nMahsulot yoki harajat nomini kiriting:'
 #     )
 
+from aiogram.dispatcher import FSMContext
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from loader import dp
+from aiogram import types
+
+son = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+
+PAGE_COUNT = 25
+
+
+def buttons_generator(current_page: int, all_pages: int, next_page=False):
+    key = InlineKeyboardMarkup(
+        row_width=3
+    )
+    if next_page:
+        key.add(InlineKeyboardButton(text="⬅️ Ortga",
+                                     callback_data="back_out_two"))
+    else:
+        key.add(InlineKeyboardButton(text="⬅️ Ortga",
+                                     callback_data="back_out"))
+
+        key.insert(InlineKeyboardButton(text=f"{current_page}/{all_pages}",
+                                        callback_data="pages"))
+
+        key.insert(InlineKeyboardButton(text="Oldinga ➡️",
+                                        callback_data="next"))
+    return key
+
+
+@dp.message_handler(text='Xabarlar', state='*')
+async def sasasample(message: types.Message, state: FSMContext):
+    all_messages = son  # db.get_all_message(_)
+
+    # PEP8
+    if all_messages:
+        current_page = 1
+        # if len(all_messages) % 25 == 0 else
+        if len(all_messages) % PAGE_COUNT == 0:
+            all_pages = len(all_messages) // PAGE_COUNT
+        else:
+            all_pages = len(all_messages) // PAGE_COUNT + 1
+        # 200 // 25
+        key = buttons_generator(all_messages[:PAGE_COUNT], current_page=current_page, all_pages=all_pages)
+        await message.answer(
+            text="Ishlayapti",
+            reply_markup=key
+        )
+        await state.update_data(
+            current_page=current_page, all_pages=all_pages
+        )
+        await state.set_state("user_select_messages")
+    else:
+        await message.answer(
+            text="Xabarlar hozircha mavjud emas"
+        )
+
+
+@dp.callback_query_handler(state="user_select_messages", text_contains="message_")
+async def get_user_details(call: types.CallbackQuery, state: FSMContext):
+    await call.answer(
+        text=f"You are selected {call.data.split('_')[1]} number",
+        show_alert=True
+    )
+
+
+@dp.callback_query_handler(state="user_select_messages")
+async def select_message_handler(call: types.CallbackQuery, state: FSMContext):
+    data = await state.get_data()
+    await call.message.delete()
+    call_data = call.data
+    current_page = data['current_page']
+    all_pages = data['all_pages']
+    if call.data == "prev":
+        if current_page == 1:
+            current_page = all_pages
+        else:
+            current_page -= 1
+    if call.data == 'next':
+        if current_page == all_pages:
+            current_page = 1
+        else:
+            current_page += 1
+
+    all_messages = son[(current_page - 1) * PAGE_COUNT: current_page * PAGE_COUNT]
+
+    key = buttons_generator(all_messages, current_page, all_pages)
+    await call.message.answer(
+        text="Ishlayapti",
+        reply_markup=key
+    )
+    await state.update_data(
+        current_page=current_page, all_pages=all_pages
+    )
--- handlers/outgoing/payhistory_out.py
+++ handlers/outgoing/payhistory_out.py
@@ -4,7 +4,9 @@ import logging
 from aiogram import types
 from aiogram.dispatcher import FSMContext
 
+from handlers.all.all_functions import next_history_page
 from handlers.all.menu_handlers import navigate
+from handlers.all.sample import buttons_generator
 from keyboards.inline.out_keyboards import back_out
 from loader import dp, db
 
@@ -17,33 +19,57 @@ async def phout_category_history(call: types.CallbackQuery, state: FSMContext):
 @dp.callback_query_handler(text_contains="historyproduct", state="*")
 async def phout_product_history(call: types.CallbackQuery, state: FSMContext):
     try:
+        # all_messages = son  # db.get_all_message(_)
+        #
+        # # PEP8
+        # if all_messages:
+        #     current_page = 1
+        #     # if len(all_messages) % 25 == 0 else
+        #     if len(all_messages) % PAGE_COUNT == 0:
+        #         all_pages = len(all_messages) // PAGE_COUNT
+        #     else:
+        #         all_pages = len(all_messages) // PAGE_COUNT + 1
+        #     # 200 // 25
+        #     key = buttons_generator(all_messages[:PAGE_COUNT], current_page=current_page, all_pages=all_pages)
+        #     await message.answer(
+        #         text="Ishlayapti",
+        #         reply_markup=key
+        #     )
+        #     await state.update_data(
+        #         current_page=current_page, all_pages=all_pages
+        #     )
+        #     await state.set_state("user_select_messages")
+        # else:
+        #     await message.answer(
+        #         text="Xabarlar hozircha mavjud emas"
+        #     )
+
         subcategory_name = call.data.split("_")[1]
         date = await db.getdate_subcategory_out(user_id=call.from_user.id,
                                                 subcategory_name=subcategory_name)
-        summary = await db.get_sum_subcategory(user_id=call.from_user.id,
-                                               subcategory_name=subcategory_name)
+
+        PAGE_COUNT = 50
 
         if date:
-            state_data = await state.get_data()
-            history = " "
+            h = " "
             c = 0
-            for data in date:
-                c += 1
-                history += f"{c}) {data[0]} | {data[1]} so'm\n"
+            s = 0
 
-                if len(history) == 4000:
-                    await call.message.edit_text(text="To'lovlaringiz tarixi ko'pligi sababli sanalar oralig'ini "
-                                                      "kiritishingizni so'raymiz!"
-                                                      "Sanalar oralig'i quyidagi tartibda kiritilishi lozim:"
-                                                      "\n\nYIL-OY-KUN ")
-                    await navigate(call=call,
-                                   callback_data=state_data,
-                                   state=state)
-                    await state.finish()
-            await call.message.edit_text(text=f"{history}\nJami: {summary} so'm",
-                                         reply_markup=back_out
-                                         )
+            for data in date[:PAGE_COUNT]:
+                c += 1
+                s += data[1]
+                h += f"{c}) {data[0]} | {data[1]} so'm\n"
+
+            if len(date) < 50:
+                await call.message.edit_text(text=f"{h}\nJami: {s} so'm",
+                                             reply_markup=back_out)
+            else:
+                key = buttons_generator(current_page=1, all_pages=2)
+                await call.message.edit_text(text=f"{h}\nJami: {s} so'm",
+                                             reply_markup=key)
+
             history = " "
+
     except Exception as err:
         logging.error(err)
 
